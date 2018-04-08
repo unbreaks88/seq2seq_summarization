b83640be82fa735d30ccf734a6d3d51cb2fb99bc
Juhwan Hong <unbreaks@Juhwanui-MacBook-Pro.local>
2018-04-08 16:20:32 +0900

    Modify crawler

--- crawler/DaumNewsCrawler.py
+++ crawler/DaumNewsCrawler.py
@@ -1,20 +1,45 @@
 # -*- coding: utf-8 -*-
-import time
-import requests
-import datetime
 import argparse
+import datetime
+import re
+import socket
+import time
+
+import requests
 from bs4 import BeautifulSoup
+
+
+def get_html(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.text
+        else:
+            return '-1'
+
+    except socket.timeout as e:
+        print(e)
+        return '-1'
+    except Exception as e:
+        print(e)
+        return '-1'
+
+
+def get_date(url):
+    m = re.search("([12]\d{3}(0[1-9]|1[0-2]))", url)
+    return m.group(0)
 
 
 def make_seed_url_list(seed_url, start_page, end_page):
     seed_list = []
     for i in range(start_page, end_page + 1):
         seed_list.append(seed_url + str(i))
+        print(seed_url + str(i))
 
     return seed_list
 
 
-def extract_news_url(html):
+def extract_news_url(html: object) -> object:
     ext_url_list = []
     soup = BeautifulSoup(html, 'html.parser')
     selected_atag = soup.select('ul.list_news2 > li > a')
@@ -25,13 +50,13 @@ def extract_news_url(html):
     return ext_url_list
 
 
-def news_data_store(category, html):
+def news_data_store(category, html, parsed_date):
     title_tag = 'h3.tit_view'
     summary_tag = 'div.news_view > .summary_view'
     document_tag = 'div.article_view'
 
     soup = BeautifulSoup(html, 'html.parser')
-    title = soup.select_one(title_tag).text.strip().replace('\n', ' ')
+
     summary = ''
     try:
         summary = soup.select_one(summary_tag).text.strip().replace('\n', ' ')
@@ -39,12 +64,12 @@ def news_data_store(category, html):
         print('not exist summary')
         return
 
+    title = soup.select_one(title_tag).text.strip().replace('\n', ' ')
     document = soup.select_one(document_tag).text.strip().replace('\n', ' ')
-
     line = "{}\t{}\t{}\t{}\n"
 
-    current = datetime.datetime.now()
-    file_name = '../data/' + category + '/' + current.strftime('%Y-%m-%d') + '.csv'
+    file_name = '../data/' + category + '/' + date + '_' + category + '.csv'
+
     with open(file_name, 'a') as fd:
         fd.write(line.format(category, title, document, summary))
     print((line.format(category, title, document, summary)))
@@ -52,12 +77,16 @@ def news_data_store(category, html):
 
 def crawl(category, seed_list, interval):
     for seed in seed_list:
-        news_url_list = extract_news_url(requests.get(seed).text)
-        for news_url in news_url_list:
-            print("crawl url : {}".format(news_url))
-            html = requests.get(news_url).text
-            news_data_store(category, html)
-            time.sleep(interval)
+        response = get_html(seed)
+        if not response.__eq__('-1'):
+            news_url_list = extract_news_url(response)
+            for news_url in news_url_list:
+                print("crawl url : {}".format(news_url))
+                parsed_date = get_date(news_url)
+                html = get_html(news_url)
+                if not response.__eq__('-1'):
+                    news_data_store(category, html, parsed_date)
+                time.sleep(interval)
 
 
 '''
@@ -71,14 +100,14 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Category')
 
     parser.add_argument('-c', '--category')
+    parser.add_argument('-d', '--date')
     args = parser.parse_args()
     category = args.category
-    print(category)
-    # category = 'society'
-    seed_url = 'http://media.daum.net/breakingnews/' + category + '?page='
-    interval = 2
+    date = args.date
+    seed_url = 'http://media.daum.net/breakingnews/' + category + '?regDate=' + date + '&page='
+    interval = 1
     start_page = 1
-    end_page = 100000;
+    end_page = 500
 
     seed_list = make_seed_url_list(seed_url, start_page, end_page)
     crawl(category, seed_list, interval)
