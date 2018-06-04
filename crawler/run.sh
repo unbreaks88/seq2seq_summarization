c18289e7a33db2dc3211616c74f8b45d50f2ab2a
unbreaks88 <unbreaks88@gmail.com>
2018-06-04 21:13:31 +0900

    Modify run.sh

--- crawler/run.sh
+++ crawler/run.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+LOG_DIR=log
+
 start_date=$1
 end_date=$2
 log=${start_date}.log
 
-echo $log
+if [ ! -d ${LOG_DIR} ]; then
+	mkdir -p ${LOG_DIR}
+fi
 
-if [ -z $start_date ] && [ -z $end_date ]; then
+if [ -z ${start_date} ] && [ -z ${end_date} ]; then
 	echo "Start_date, end_date must be needed"
 	exit -1
 fi
 
-nohup python3 news_crawler.py -i 0.01 -s $start_date -e $end_date > $log 2>&1
+nohup python3 news_crawler.py -i 0.01 -s $start_date -e $end_date &> ${LOG_DIR}/${log}&
